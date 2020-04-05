4ad5db34af032d3f7ce0a077f9956a19de538e24
Marco Ottolini <marco@ottolini.com>
2020-04-05 08:27:09 +0200

    able to create json for umap

--- processing/index.js
+++ processing/index.js
@@ -12,7 +12,33 @@ csv()
     for (let i=0; i<locations.length; i++) {
       const loc = locations[i];
       //console.log(loc);
-      ret.features.push(loc);
+      const ratio = loc.deaths / loc.population;
+      let f = {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [
+            parseFloat(loc.lng),
+            parseFloat(loc.lat)
+          ]
+        },
+        properties: {
+          _umap_options: {
+            color: (ratio > 0.01) ? "Red" : "Green",
+            iconClass: "Drop", //possible values: "Circle", "Default", "Drop", "Ball"
+            //iconUrl: "/uploads/pictogram/airport-24.png"
+          },
+          name: loc.name,
+          population: loc.population,
+          deaths: loc.deaths,
+          ratio: ratio,
+          "marker-color": "#ff0000",  //geojson.io
+          "marker-size": "medium",    //geojson.io
+          "marker-symbol": "triangle" //geojson.io
+        } /*,
+        loc: loc */
+      };
+      ret.features.push(f);
     }
     console.log(JSON.stringify(ret));
     /**
